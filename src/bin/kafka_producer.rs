035d72f65e45cba087df74d1ae49c2bb49cf2d93
Theodlz <theophile.dulaz@gmail.com>
2025-02-11 09:00:15 -0800

    use clap in kafka_producer bin

--- src/bin/kafka_producer.rs
+++ src/bin/kafka_producer.rs
@@ -1,7 +1,20 @@
 use boom::kafka::produce_from_archive;
-use std::env;
+use clap::Parser;
 use tracing::{error, info, Level};
 use tracing_subscriber::FmtSubscriber;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(help = "Date of archival alerts to produce, with format YYYYMMDD. Defaults to today.")]
+    date: Option<String>,
+
+    #[arg(
+        long,
+        value_name = "LIMIT",
+        help = "Limit the number of alerts produced"
+    )]
+    limit: Option<i64>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -11,21 +24,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
-    let args: Vec<String> = env::args().collect();
+    let args = Cli::parse();
     let mut date = chrono::Utc::now().format("%Y%m%d").to_string();
     let mut limit = 0;
-    if args.len() > 1 {
-        let arg = &args[1];
-        if arg.len() == 8 {
-            info!("Using date from argument: {}", arg);
-            date = arg.to_string();
+
+    if let Some(d) = args.date {
+        if d.len() == 8 {
+            info!("Using date from argument: {}", d);
+            date = d;
         } else {
-            error!("Invalid date format: {}", arg);
+            error!("Invalid date format: {}", d);
             return Ok(());
         }
     }
-    if args.len() > 2 {
-        limit = args[2].parse::<i64>().unwrap_or(0);
+    if let Some(l) = args.limit {
+        limit = l;
     }
 
     let _ = produce_from_archive(&date, limit).await.unwrap();
