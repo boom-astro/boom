411e213910f900b849c4ab0bf697ec1c294cf32e
Jake Simones <simonj0@icloud.com>
2025-03-02 20:23:15 -0600

    handle index creation errors (#78)

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,15 @@
-use tracing::error;
+use tracing::instrument;
 
+#[derive(thiserror::Error, Debug)]
+#[error("failed to create index")]
+pub struct CreateIndexError(#[from] mongodb::error::Error);
+
+#[instrument(skip(collection, index), err, fields(collection = collection.name()))]
 pub async fn create_index(
     collection: &mongodb::Collection<mongodb::bson::Document>,
     index: mongodb::bson::Document,
     unique: bool,
-) -> mongodb::error::Result<()> {
+) -> Result<(), CreateIndexError> {
     let index_model = mongodb::IndexModel::builder()
         .keys(index)
         .options(
@@ -13,15 +18,6 @@ pub async fn create_index(
                 .build(),
         )
         .build();
-    match collection.create_index(index_model).await {
-        Err(e) => {
-            error!(
-                "Error when creating index for collection {}: {}",
-                collection.name(),
-                e
-            );
-        }
-        Ok(_x) => {}
-    }
+    collection.create_index(index_model).await?;
     Ok(())
 }
