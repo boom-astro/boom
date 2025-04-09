d4de329b32db3f295be833f61bddaffff09b3fd0
Theodlz <theophile.dulaz@gmail.com>
2025-04-08 18:56:37 -0700

    just like for the alert and filter workers, move the redis pull/push and terminal command check queue out of the surveys ml workers and to the generic worker trait's run_ml_worker function

--- src/ml/base.rs
+++ src/ml/base.rs
@@ -1,6 +1,10 @@
 use crate::{conf, utils::worker::WorkerCmd};
 use mongodb::bson::Document;
+use redis::AsyncCommands;
+use std::num::NonZero;
 use tokio::sync::mpsc;
+use tokio::sync::mpsc::error::TryRecvError;
+use tracing::{error, info, warn};
 
 #[derive(thiserror::Error, Debug)]
 pub enum MLWorkerError {
@@ -10,31 +14,96 @@ pub enum MLWorkerError {
     ConnectRedisError(#[from] redis::RedisError),
     #[error("failed to read config")]
     ReadConfigError(#[from] conf::BoomConfigError),
+    #[error("failed to pop candids from the input queue")]
+    PopCandidsError(#[source] redis::RedisError),
+    #[error("failed to push candids to the output queue")]
+    PushCandidsError(#[source] redis::RedisError),
 }
 
 #[async_trait::async_trait]
 pub trait MLWorker {
-    async fn new(
-        id: String,
-        receiver: mpsc::Receiver<WorkerCmd>,
-        config_path: &str,
-    ) -> Result<Self, MLWorkerError>
+    async fn new(config_path: &str) -> Result<Self, MLWorkerError>
     where
         Self: Sized;
+    fn input_queue_name(&self) -> String;
+    fn output_queue_name(&self) -> String;
     async fn fetch_alerts(
         &self,
         candids: &[i64], // this is a slice of candids to process
     ) -> Result<Vec<Document>, MLWorkerError>;
-    async fn run(&mut self) -> Result<(), MLWorkerError>;
+    async fn process_alerts(&self, alerts: &[i64]) -> Result<Vec<String>, MLWorkerError>;
 }
 
 #[tokio::main]
 pub async fn run_ml_worker<T: MLWorker>(
     id: String,
-    receiver: mpsc::Receiver<WorkerCmd>,
+    mut receiver: mpsc::Receiver<WorkerCmd>,
     config_path: &str,
 ) -> Result<(), MLWorkerError> {
-    let mut ml_worker = T::new(id, receiver, config_path).await?;
-    ml_worker.run().await?;
+    let ml_worker = T::new(config_path).await?;
+
+    // in a never ending loop, loop over the queues
+    let client_redis = redis::Client::open("redis://localhost:6379".to_string())
+        .map_err(MLWorkerError::ConnectRedisError)?;
+    let mut con = client_redis
+        .get_multiplexed_async_connection()
+        .await
+        .map_err(MLWorkerError::ConnectRedisError)?;
+
+    let input_queue = ml_worker.input_queue_name();
+    let output_queue = ml_worker.output_queue_name();
+
+    let command_interval: i64 = 500;
+    let mut command_check_countdown = command_interval;
+
+    loop {
+        if command_check_countdown == 0 {
+            match receiver.try_recv() {
+                Ok(WorkerCmd::TERM) => {
+                    info!("alert worker {} received termination command", &id);
+                    break;
+                }
+                Err(TryRecvError::Disconnected) => {
+                    warn!("alert worker {} receiver disconnected, terminating", &id);
+                    break;
+                }
+                Err(TryRecvError::Empty) => {
+                    command_check_countdown = command_interval;
+                }
+            }
+        }
+        // if the queue is empty, wait for a bit and continue the loop
+        let queue_len: i64 = con
+            .llen(&input_queue)
+            .await
+            .map_err(MLWorkerError::ConnectRedisError)?;
+        if queue_len == 0 {
+            tokio::time::sleep(std::time::Duration::from_millis(500)).await;
+            continue;
+        }
+
+        // get candids from redis
+        let candids: Vec<i64> = con
+            .rpop::<&str, Vec<i64>>(&input_queue, NonZero::new(1000))
+            .await
+            .map_err(MLWorkerError::PopCandidsError)?;
+
+        let nb_candids = candids.len();
+        if nb_candids == 0 {
+            // sleep for a bit if no alerts were found
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+            continue;
+        }
+
+        let processed_alerts = ml_worker.process_alerts(&candids).await?;
+
+        // push that back to redis to the output queue
+        con.lpush::<&str, Vec<String>, usize>(&output_queue, processed_alerts)
+            .await
+            .map_err(MLWorkerError::PushCandidsError)?;
+
+        command_check_countdown -= nb_candids as i64;
+    }
+
     Ok(())
 }
--- src/ml/ztf.rs
+++ src/ml/ztf.rs
@@ -1,40 +1,64 @@
-use crate::utils::worker::WorkerCmd;
-use mongodb::options::{UpdateOneModel, WriteModel};
-use redis::AsyncCommands;
-use std::num::NonZero;
-use tokio::sync::mpsc;
-use tokio::sync::mpsc::error::TryRecvError;
-use tracing::{info, warn};
-
 use crate::ml::models::{AcaiModel, BtsBotModel, Model};
 use crate::ml::{MLWorker, MLWorkerError};
 use futures::StreamExt;
 use mongodb::bson::{doc, Document};
+use mongodb::options::{UpdateOneModel, WriteModel};
+use tracing::warn;
 
 pub struct ZtfMLWorker {
-    id: String,
-    receiver: mpsc::Receiver<WorkerCmd>,
+    input_queue: String,
+    output_queue: String,
     client: mongodb::Client,
     alert_collection: mongodb::Collection<mongodb::bson::Document>,
+    acai_h_model: AcaiModel,
+    acai_n_model: AcaiModel,
+    acai_v_model: AcaiModel,
+    acai_o_model: AcaiModel,
+    acai_b_model: AcaiModel,
+    btsbot_model: BtsBotModel,
 }
 
 #[async_trait::async_trait]
 impl MLWorker for ZtfMLWorker {
-    async fn new(
-        id: String,
-        receiver: mpsc::Receiver<WorkerCmd>,
-        config_path: &str,
-    ) -> Result<Self, MLWorkerError> {
+    async fn new(config_path: &str) -> Result<Self, MLWorkerError> {
         let config_file = crate::conf::load_config(&config_path)?;
         let db: mongodb::Database = crate::conf::build_db(&config_file).await?;
         let client = db.client().clone();
         let alert_collection = db.collection("ZTF_alerts");
+
+        let input_queue = "ZTF_alerts_classifier_queue".to_string();
+        let output_queue = "ZTF_alerts_filter_queue".to_string();
+
+        // we load the ACAI models (same architecture, same input/output)
+        let acai_h_model = AcaiModel::new("data/models/acai_h.d1_dnn_20201130.onnx");
+        let acai_n_model = AcaiModel::new("data/models/acai_n.d1_dnn_20201130.onnx");
+        let acai_v_model = AcaiModel::new("data/models/acai_v.d1_dnn_20201130.onnx");
+        let acai_o_model = AcaiModel::new("data/models/acai_o.d1_dnn_20201130.onnx");
+        let acai_b_model = AcaiModel::new("data/models/acai_b.d1_dnn_20201130.onnx");
+
+        // we load the btsbot model (different architecture, and input/output then ACAI)
+        let btsbot_model = BtsBotModel::new("data/models/btsbot-v1.0.1.onnx");
+
         Ok(ZtfMLWorker {
-            id,
-            receiver,
+            input_queue,
+            output_queue,
             client,
             alert_collection,
+            acai_h_model,
+            acai_n_model,
+            acai_v_model,
+            acai_o_model,
+            acai_b_model,
+            btsbot_model,
         })
+    }
+
+    fn input_queue_name(&self) -> String {
+        self.input_queue.clone()
+    }
+
+    fn output_queue_name(&self) -> String {
+        self.output_queue.clone()
     }
 
     async fn fetch_alerts(
@@ -152,148 +176,70 @@ impl MLWorker for ZtfMLWorker {
         Ok(alerts)
     }
 
-    async fn run(&mut self) -> Result<(), MLWorkerError> {
-        let client_redis = redis::Client::open("redis://localhost:6379".to_string()).unwrap();
-        let mut con = client_redis
-            .get_multiplexed_async_connection()
-            .await
-            .map_err(MLWorkerError::ConnectRedisError)?;
+    async fn process_alerts(&self, candids: &[i64]) -> Result<Vec<String>, MLWorkerError> {
+        let alerts = self.fetch_alerts(&candids).await.unwrap();
 
-        let queue = "ZTF_alerts_classifier_queue".to_string();
-        let output_queue = "ZTF_alerts_filter_queue".to_string();
-
-        let command_interval: i64 = 500;
-        let mut command_check_countdown = command_interval;
-        let mut count = 0;
-
-        // we load the ACAI models (same architecture, same input/output)
-        let acai_h_model = AcaiModel::new("data/models/acai_h.d1_dnn_20201130.onnx");
-        let acai_n_model = AcaiModel::new("data/models/acai_n.d1_dnn_20201130.onnx");
-        let acai_v_model = AcaiModel::new("data/models/acai_v.d1_dnn_20201130.onnx");
-        let acai_o_model = AcaiModel::new("data/models/acai_o.d1_dnn_20201130.onnx");
-        let acai_b_model = AcaiModel::new("data/models/acai_b.d1_dnn_20201130.onnx");
-
-        // we load the btsbot model (different architecture, and input/output then ACAI)
-        let btsbot_model = BtsBotModel::new("data/models/btsbot-v1.0.1.onnx");
-
-        let start = std::time::Instant::now();
-        loop {
-            // check for command from threadpool
-            if command_check_countdown == 0 {
-                match self.receiver.try_recv() {
-                    Ok(WorkerCmd::TERM) => {
-                        info!("alert worker {} received termination command", self.id);
-                        break;
-                    }
-                    Err(TryRecvError::Disconnected) => {
-                        warn!(
-                            "alert worker {} receiver disconnected, terminating",
-                            self.id
-                        );
-                        break;
-                    }
-                    Err(TryRecvError::Empty) => {
-                        command_check_countdown = command_interval;
-                    }
-                }
-            }
-
-            let candids = con
-                .rpop::<&str, Vec<i64>>(&queue, NonZero::new(1000))
-                .await
-                .unwrap();
-
-            if candids.is_empty() {
-                info!("ML WORKER {}: Queue is empty", self.id);
-                tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
-                command_check_countdown = 0;
-                continue;
-            }
-
-            let alerts = self.fetch_alerts(&candids).await.unwrap();
-
-            if alerts.len() != candids.len() {
-                warn!(
-                    "ML WORKER {}: only {} alerts fetched from {} candids",
-                    self.id,
-                    alerts.len(),
-                    candids.len()
-                );
-            }
-
-            // we keep it very simple for now, let's run on 1 alert at a time
-            // we will move to batch processing later
-            let mut updates = Vec::new();
-            let mut processed_candids = Vec::new();
-            for i in 0..alerts.len() {
-                let candid = alerts[i].get_i64("_id").unwrap();
-                let programid = alerts[i]
-                    .get_document("candidate")
-                    .unwrap()
-                    .get_i32("programid")
-                    .unwrap();
-
-                let metadata = acai_h_model.get_metadata(&alerts[i..i + 1]);
-                let triplet = acai_h_model.get_triplet(&alerts[i..i + 1]);
-
-                let acai_h_scores = acai_h_model.predict(&metadata, &triplet);
-                let acai_n_scores = acai_n_model.predict(&metadata, &triplet);
-                let acai_v_scores = acai_v_model.predict(&metadata, &triplet);
-                let acai_o_scores = acai_o_model.predict(&metadata, &triplet);
-                let acai_b_scores = acai_b_model.predict(&metadata, &triplet);
-
-                let metadata_btsbot = btsbot_model.get_metadata(&alerts[i..i + 1]);
-                let btsbot_scores = btsbot_model.predict(&metadata_btsbot, &triplet);
-
-                let find_document = doc! {
-                    "_id": candid
-                };
-
-                let update_alert_document = doc! {
-                    "$set": {
-                        "classifications.acai_h": acai_h_scores[0],
-                        "classifications.acai_n": acai_n_scores[0],
-                        "classifications.acai_v": acai_v_scores[0],
-                        "classifications.acai_o": acai_o_scores[0],
-                        "classifications.acai_b": acai_b_scores[0],
-                        "classifications.btsbot": btsbot_scores[0]
-                    }
-                };
-
-                let update = WriteModel::UpdateOne(
-                    UpdateOneModel::builder()
-                        .namespace(self.alert_collection.namespace())
-                        .filter(find_document)
-                        .update(update_alert_document)
-                        .build(),
-                );
-
-                updates.push(update);
-                processed_candids.push((candid, programid));
-            }
-
-            if count % 1000 == 0 {
-                let elapsed = start.elapsed().as_secs();
-                info!(
-                    "\nRan ML models for {} ZTF alerts in {} seconds, avg: {:.4} alerts/s\n",
-                    count,
-                    elapsed,
-                    count as f64 / elapsed as f64
-                );
-            }
-
-            let nb_modified = self.client.bulk_write(updates).await?.modified_count;
-
-            // TODO: once PR https://github.com/boom-astro/boom/pull/97
-            // is merged, push a string tuple and not a tuple,
-            // which isn't valid and just pushes 2 individual values to redis
-            con.lpush::<&str, Vec<(i64, i32)>, usize>(&output_queue, processed_candids)
-                .await?;
-
-            count += nb_modified;
-            command_check_countdown -= nb_modified;
+        if alerts.len() != candids.len() {
+            warn!(
+                "ML WORKER: only {} alerts fetched from {} candids",
+                alerts.len(),
+                candids.len()
+            );
         }
 
-        Ok(())
+        // we keep it very simple for now, let's run on 1 alert at a time
+        // we will move to batch processing later
+        let mut updates = Vec::new();
+        let mut processed_alerts = Vec::new();
+        for i in 0..alerts.len() {
+            let candid = alerts[i].get_i64("_id").unwrap();
+            let programid = alerts[i]
+                .get_document("candidate")
+                .unwrap()
+                .get_i32("programid")
+                .unwrap();
+
+            let metadata = self.acai_h_model.get_metadata(&alerts[i..i + 1]);
+            let triplet = self.acai_h_model.get_triplet(&alerts[i..i + 1]);
+
+            let acai_h_scores = self.acai_h_model.predict(&metadata, &triplet);
+            let acai_n_scores = self.acai_n_model.predict(&metadata, &triplet);
+            let acai_v_scores = self.acai_v_model.predict(&metadata, &triplet);
+            let acai_o_scores = self.acai_o_model.predict(&metadata, &triplet);
+            let acai_b_scores = self.acai_b_model.predict(&metadata, &triplet);
+
+            let metadata_btsbot = self.btsbot_model.get_metadata(&alerts[i..i + 1]);
+            let btsbot_scores = self.btsbot_model.predict(&metadata_btsbot, &triplet);
+
+            let find_document = doc! {
+                "_id": candid
+            };
+
+            let update_alert_document = doc! {
+                "$set": {
+                    "classifications.acai_h": acai_h_scores[0],
+                    "classifications.acai_n": acai_n_scores[0],
+                    "classifications.acai_v": acai_v_scores[0],
+                    "classifications.acai_o": acai_o_scores[0],
+                    "classifications.acai_b": acai_b_scores[0],
+                    "classifications.btsbot": btsbot_scores[0]
+                }
+            };
+
+            let update = WriteModel::UpdateOne(
+                UpdateOneModel::builder()
+                    .namespace(self.alert_collection.namespace())
+                    .filter(find_document)
+                    .update(update_alert_document)
+                    .build(),
+            );
+
+            updates.push(update);
+            processed_alerts.push(format!("{},{}", programid, candid));
+        }
+
+        let _ = self.client.bulk_write(updates).await?.modified_count;
+
+        Ok(processed_alerts)
     }
 }
