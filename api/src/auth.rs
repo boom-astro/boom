72981c7f45056cb940ec9a3212a7367a16fdeb56
Theodlz <theophile.dulaz@gmail.com>
2025-06-10 10:36:31 -0700

    add authentication provider using jwt, auth middleware to protect all endpoints except the newly added /auth route to retrieve a jwt from username/password

---
+++ api/src/auth.rs
@@ -0,0 +1,145 @@
+use mongodb::bson::doc;
+
+use crate::{api::users::User, conf::AppConfig, conf::AuthConfig};
+
+use jsonwebtoken::{Algorithm, DecodingKey, EncodingKey, Header, Validation, decode, encode};
+use serde::{Deserialize, Serialize};
+
+/// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Claims {
+    sub: String,
+    iat: usize,
+    exp: usize,
+}
+
+#[derive(Clone)]
+pub struct AuthProvider {
+    encoding_key: EncodingKey,
+    decoding_key: DecodingKey,
+    validation: Validation,
+    users_collection: mongodb::Collection<User>,
+    token_expiration: usize,
+}
+
+impl AuthProvider {
+    pub async fn new(config: AuthConfig, db: &mongodb::Database) -> Self {
+        let encoding_key = EncodingKey::from_secret(config.secret_key.as_bytes());
+        let decoding_key = DecodingKey::from_secret(config.secret_key.as_bytes());
+        let mut validation = Validation::new(Algorithm::HS256);
+        validation.validate_exp = config.token_expiration > 0; // Set to true if tokens should expire
+
+        let users_collection: mongodb::Collection<User> = db.collection("users");
+
+        // always create the admin user if it doesn't exist
+        // using the admin_username from the config and admin_password
+        let admin_username = config.admin_username.clone();
+        // first check if the admin user already exists
+        match users_collection
+            .find_one(doc! { "username": &admin_username })
+            .await
+        {
+            Ok(Some(_)) => {
+                // Admin user already exists
+            }
+            Ok(None) => {
+                // Admin user does not exist, create it
+                let user_id = uuid::Uuid::new_v4().to_string();
+                let admin_password = config.admin_password.clone();
+                let hashed_admin_password = bcrypt::hash(&admin_password, bcrypt::DEFAULT_COST)
+                    .expect("failed to hash admin password");
+                let admin_user = User {
+                    id: user_id,
+                    username: admin_username.clone(),
+                    email: "<admin_email@example.com>".to_string(),
+                    password: hashed_admin_password,
+                };
+                // DEBUG print the username, password
+                println!("Creating admin user:");
+                println!("Username: {}", &admin_username);
+                println!("Password: {}", &admin_password);
+                match users_collection.insert_one(admin_user).await {
+                    Ok(_) => {
+                        println!("Admin user created successfully.");
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to create admin user: {}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Failed to check for admin user: {}", e);
+            }
+        }
+
+        AuthProvider {
+            encoding_key,
+            decoding_key,
+            validation,
+            users_collection,
+            token_expiration: config.token_expiration,
+        }
+    }
+
+    pub async fn create_token(&self, user: &User) -> Result<String, jsonwebtoken::errors::Error> {
+        let iat = chrono::Utc::now().timestamp() as usize;
+        let exp = iat + self.token_expiration;
+        let claims = Claims {
+            sub: user.id.clone(),
+            iat,
+            exp,
+        };
+
+        encode(&Header::default(), &claims, &self.encoding_key)
+    }
+
+    pub async fn decode_token(&self, token: &str) -> Result<Claims, jsonwebtoken::errors::Error> {
+        decode::<Claims>(token, &self.decoding_key, &self.validation).map(|data| data.claims)
+    }
+
+    pub async fn validate_token(&self, token: &str) -> Result<String, jsonwebtoken::errors::Error> {
+        let claims = self.decode_token(token).await?;
+        Ok(claims.sub)
+    }
+
+    pub async fn authenticate_user(
+        &self,
+        username: &str,
+        password: &str,
+    ) -> Result<String, std::io::Error> {
+        let filter = mongodb::bson::doc! { "username": username };
+        let user = self.users_collection.find_one(filter).await.map_err(|e| {
+            std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("Database query failed: {}", e),
+            )
+        })?;
+
+        // if the user exists and the password matches, create a token
+        // otherwise return an error
+        if let Some(user) = user {
+            match bcrypt::verify(&password, &user.password) {
+                Ok(true) => self.create_token(&user).await.map_err(|e| {
+                    std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        format!("Token creation failed: {}", e),
+                    )
+                }),
+                _ => Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "Invalid credentials",
+                )),
+            }
+        } else {
+            Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "User not found",
+            ))
+        }
+    }
+}
+
+pub async fn get_auth(db: &mongodb::Database) -> AuthProvider {
+    let config = AppConfig::from_default_path().auth;
+    AuthProvider::new(config, db).await
+}
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,7 +1,38 @@
+use actix_web::body::MessageBody;
+use actix_web::dev::{ServiceRequest, ServiceResponse};
+use actix_web::middleware::{Next, from_fn};
+use actix_web::{App, Error, HttpResponse, HttpServer, get, middleware::Logger, web};
 use boom_api::api;
+use boom_api::auth::{AuthProvider, get_auth};
 use boom_api::db::get_db;
 
-use actix_web::{App, HttpResponse, HttpServer, get, middleware::Logger, web};
+async fn auth_middleware(
+    req: ServiceRequest,
+    next: Next<impl MessageBody>,
+) -> Result<ServiceResponse<impl MessageBody>, Error> {
+    let auth_app_data: &web::Data<AuthProvider> = req.app_data().unwrap();
+    match req.headers().get("Authorization") {
+        Some(auth_header) if auth_header.to_str().unwrap_or("").starts_with("Bearer ") => {
+            let token = auth_header.to_str().unwrap()[7..].trim();
+            match auth_app_data.validate_token(token).await {
+                Ok(user_id) => {
+                    println!("Valid token for user ID: {}", user_id);
+                }
+                Err(e) => {
+                    println!("Invalid token: {}", e);
+                    return Err(actix_web::error::ErrorUnauthorized("Invalid token"));
+                }
+            }
+        }
+        _ => {
+            println!("No valid Authorization header");
+            return Err(actix_web::error::ErrorUnauthorized(
+                "Missing or invalid Authorization header",
+            ));
+        }
+    }
+    next.call(req).await
+}
 
 #[get("/")]
 pub async fn get_health() -> HttpResponse {
@@ -14,6 +45,7 @@ pub async fn get_health() -> HttpResponse {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let database = get_db().await;
+    let auth = get_auth(&database).await;
 
     // Initialize logging
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
@@ -21,18 +53,23 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(database.clone()))
-            .service(get_health)
-            .service(api::query::get_info)
-            .service(api::query::sample)
-            .service(api::query::cone_search)
-            .service(api::query::count_documents)
-            .service(api::query::find)
-            .service(api::alerts::get_object)
-            .service(api::filters::post_filter)
-            .service(api::filters::add_filter_version)
-            .service(api::users::post_user)
-            .service(api::users::get_users)
-            .service(api::users::delete_user)
+            .app_data(web::Data::new(auth.clone()))
+            .service(api::auth::post_auth)
+            .service(
+                actix_web::web::scope("")
+                    .wrap(from_fn(auth_middleware))
+                    .service(api::query::get_info)
+                    .service(api::query::sample)
+                    .service(api::query::cone_search)
+                    .service(api::query::count_documents)
+                    .service(api::query::find)
+                    .service(api::alerts::get_object)
+                    .service(api::filters::post_filter)
+                    .service(api::filters::add_filter_version)
+                    .service(api::users::post_user)
+                    .service(api::users::get_users)
+                    .service(api::users::delete_user),
+            )
             .wrap(Logger::default())
     })
     .bind(("0.0.0.0", 4000))?
