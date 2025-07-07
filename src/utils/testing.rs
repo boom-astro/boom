ea49ce327cc05f9501298de4027816e435e70823
Theodlz <theophile.dulaz@gmail.com>
2025-07-06 21:52:40 -0700

    handle missing survey enum case in testing.rs

--- src/utils/testing.rs
+++ src/utils/testing.rs
@@ -224,6 +224,7 @@ impl AlertRandomizer {
                     Some(SchemaRegistry::new(LSST_SCHEMA_REGISTRY_URL)),
                 )
             }
+            _ => panic!("Unsupported survey for randomization"),
         };
         let candid = Some(rand::rng().random_range(0..i64::MAX));
         let ra = Some(rand::rng().random_range(0.0..360.0));
@@ -301,6 +302,7 @@ impl AlertRandomizer {
                 object_id
             }
             Survey::Lsst => format!("{}", rand::rng().random_range(0..i64::MAX)),
+            _ => panic!("Unsupported survey for randomization"),
         }
     }
 
@@ -532,6 +534,7 @@ impl AlertRandomizer {
                     new_payload,
                 )
             }
+            _ => panic!("Unsupported survey for randomization"),
         }
     }
 
