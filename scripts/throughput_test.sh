db93c333fc933a0ffe88dc33df933e2a9ffbf685
jesaerys <simonj0@icloud.com>
2025-07-11 22:28:16 -0500

    report max queue length and corresponding time
    This information helps determine whether the overall reported throughput is limited by the consumer or the scheduler.

--- scripts/throughput_test.sh
+++ scripts/throughput_test.sh
@@ -6,22 +6,9 @@ usage() {
 
 help() {
   cat <<EOF
+Perform a boom throughput test in a *non-production* environment
+
 Usage: $(usage)
-
-Perform a boom throughput test in a *non-production* environment.
-
-This utility does the following:
-
-1.  Start kafka, valkey, and mongodb in docker if they aren't already running.
-2.  Run the producer to create the kafka topic for <SURVEY> and <DATE> if it
-    does not yet exist.
-3.  Count the total number of alerts in the topic.
-4.  Repeat the following test <N> times (see '--iterations'):
-    -   Drop the 'boom' database in mongodb.
-    -   Start the consumer and scheduler in the background.
-    -   Periodically count the number of alerts in 'boom' and stop the consumer
-        and scheduler when the expected number is reached.
-    -   Record the duration from when the consumer started to this point.
 
 Arguments:
   <SURVEY>  Survey name (e.g., ztf)
@@ -29,18 +16,51 @@ Arguments:
 
 Options:
   -i, --iterations <N>    Repeat the test N > 0 times [default: ${iterations}]
-      --producer <PATH>   Path to the producer binary [default: ${producer}]
-      --consumer <PATH>   Path to the consumer binary [default: ${consumer}]
-      --scheduler <PATH>  Path to the scheduler binary [default: ${scheduler}]
+      --producer <CMD>    Base command for the producer [default: ${producer}]
+      --consumer <CMD>    Base command for the consumer [default: ${consumer}]
+      --scheduler <CMD>   Base command for the scheduler [default: ${scheduler}]
       --timeout <SEC>     Timeout for counting the alerts in the kafka topic
                           (may want to increase this for dates with a large
                           number of alerts) [default: ${timeout}]
-  -o, --output <PATH>     Append test results to the given file.
+  -o, --output <PATH>     Write throughput values to the given file.
   -h, --help              Print help
 
 Environment variables:
   MONGO_USERNAME  MongoDB username
   MONGO_PASSWORD  MongoDB password
+
+Discussion:
+    This utility does the following:
+
+    1.  Start kafka, valkey, and mongodb in docker if they aren't already
+        running.
+    2.  Run the producer to create the kafka topic for <SURVEY> and <DATE> if
+        it does not yet exist.
+    3.  Count the total number of alerts in the topic.
+    4.  Repeat the following test <N> times (see '--iterations'):
+        -   Drop the 'boom' database in mongodb.
+        -   Start the consumer and scheduler in the background.
+        -   Periodically count the number of alerts in 'boom' and stop the
+            consumer and scheduler when the expected number is reached.
+        -   Record the time taken to process the alerts.
+
+    Each test iteration concludes with a short report of results including,
+
+    - The number of alerts (n)
+    - The total processing time (t; from when the consumer started to when the
+      last alert landed in the database)
+    - The throughput rate (r = n / t)
+    - The peak number of alerts that were in valkey queue (n_peak)
+    - The time when the peak occurred (t_peak)
+
+    The peak measurements are useful because they indicate how the throughput
+    of the consumer (r_c) compares to the throughput of the scheduler (r_s).
+    In essence, if n_peak and t_peak are unambiguously greater than zero and
+    less than t, respectively, then the consumer was faster than the scheduler
+    (r_c > r_s) and the overall throughput was limited by scheduler, i.e.,
+    r = r_s. If instead n_peak is at or near zero and t_peak is approximately
+    equal to t, then the consumer was slower than the scheduler (r_c < r_s) and
+    the overall throughput was limited by the consumer, i.e., r = r_c.
 EOF
 }
 
@@ -178,7 +198,7 @@ run_producer() {
   local date="${3:?}"
 
   info "running the producer"
-  "${producer}" "${survey}" "${date}" || {
+  ${producer} "${survey}" "${date}" || {
     error "failed to run the producer"
     return 1
   }
@@ -227,7 +247,7 @@ start_consumer() {
   local date="${3:?}"
 
   info "starting the consumer"
-  "${consumer}" --clear "${survey}" "${date}" >&2 &
+  ${consumer} --clear "${survey}" "${date}" >&2 &
   echo "$!"
 }
 
@@ -236,7 +256,7 @@ start_scheduler() {
   local survey="${2:?}"
 
   info "starting the scheduler"
-  "${scheduler}" "${survey}" >&2 &
+  ${scheduler} "${survey}" >&2 &
   echo "$!"
 }
 
@@ -247,21 +267,48 @@ wait_for_scheduler() {
 
   local alert_collection_name
   alert_collection_name="$(echo "${survey}" | tr '[:lower:]' '[:upper:]')_alerts"
+  local alert_queue_name
+  alert_queue_name="$(echo "${survey}" | tr '[:lower:]' '[:upper:]')_alerts_packets_queue"
 
   local elapsed
   local count
+  local nprev=0  # Previous number of alerts in the queue
+  local ncurr=0  # Current number of alerts in the queue
+  local nmax=0  # Max number of alerts observed in the queue
+  local tmax=0  # Time at nmax
+  local decreasing=false
   while true; do
-    count=$(docker exec boom-mongo-1 mongosh \
+    # Count the number of alerts in the queue
+    ncurr="$(docker exec boom-valkey-1 redis-cli LLEN "${alert_queue_name}")" && {
+      if ((ncurr > nprev)); then  # The queue is increasing
+        if "${decreasing}"; then  # The queue just started increasing
+          decreasing=false
+        fi
+      else  # The queue is decreasing
+        if ! "${decreasing}"; then  # The queue just started decreasing
+          decreasing=true
+          nmax="${nprev}"
+          tmax="$(date +%s)"
+        fi
+      fi
+    } || {
+      warn "failed to get the queue length"
+    }
+    nprev="${ncurr}"
+
+    # Count the number of alerts in mongodb
+    count="$(docker exec boom-mongo-1 mongosh \
       --username "${MONGO_USERNAME}" \
       --password "${MONGO_PASSWORD}" \
       --authenticationDatabase admin \
       --quiet \
       --eval "db.getSiblingDB('boom').${alert_collection_name}.countDocuments()"
-    ) || return 1
+    )" || return 1
     elapsed=$(($(date +%s) - start))
+
     info "${count} alerts processed in ${elapsed} seconds"
     if [[ "${count}" -ge "${expected_count}" ]]; then
-      echo "${count} ${elapsed}"
+      echo "${count} ${elapsed} ${nmax} $((tmax - start))"
       break
     fi
 
@@ -397,6 +444,8 @@ main() {
   local results
   local count
   local elapsed
+  local nmax
+  local tmax
   local rate
   while true; do
     i=$((i + 1))
@@ -412,14 +461,15 @@ main() {
         "${date}" \
         "${expected_count}"
     )" || continue
-    count="${results% *}"
-    elapsed="${results#* }"
+    read count elapsed nmax tmax <<<"${results}"
     rate="$(echo "scale=6; ${count} / ${elapsed}" | bc)"
 
     info "test ${i} results:
     number of alerts:         ${count}
     processing time (sec):    ${elapsed}
-    throughput (alerts/sec):  ${rate}"
+    throughput (alerts/sec):  ${rate}
+    peak queue length:        ${nmax}
+    time at peak (sec):       ${tmax}"
 
     if [[ -n ${output} ]]; then
       echo "${rate}" >>"${output}"
