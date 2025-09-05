a135e21e76d30debcbe77f36fffe0565c9dbf3b8
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-09-05 19:45:03 +0000

    Fix path.

--- tests/apptainer/def/build-tests-sif.sh
+++ tests/apptainer/def/build-tests-sif.sh
@@ -1,5 +1,5 @@
 mkdir -p tests/apptainer/sif
-apptainer build apptainer/sif/mongo.sif apptainer/def/mongo.def
-apptainer build apptainer/sif/valkey.sif apptainer/def/valkey.def
-apptainer build apptainer/sif/kafka.sif apptainer/def/kafka.def
-apptainer build apptainer/sif/boom-benchmarking.sif apptainer/def/boom-benchmarking.def
+apptainer build tests/apptainer/sif/mongo.sif tests/apptainer/def/mongo.def
+apptainer build tests/apptainer/sif/valkey.sif tests/apptainer/def/valkey.def
+apptainer build tests/apptainer/sif/kafka.sif tests/apptainer/def/kafka.def
+apptainer build tests/apptainer/sif/boom-benchmarking.sif tests/apptainer/def/boom-benchmarking.def
