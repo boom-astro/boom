4cd4da99069b29e8a3c570cfdc04b5f2e02b2a32
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-09-07 21:54:19 +0000

    Improve apptainer compose, add monitoring services.

--- apptainer_compose.sh
+++ apptainer_compose.sh
@@ -3,11 +3,10 @@
 BOOM_DIR="$HOME/boom"
 
 LOGS_DIR=${1:-$BOOM_DIR/logs/boom}
-CONFIG_FILE="$BOOM_DIR/config.yaml"
-
-SCRIPTS_DIR="$BOOM_DIR/apptainer/scripts"
-SIF_DIR="$BOOM_DIR/apptainer/sif"
 PERSISTENT_DIR="$BOOM_DIR/apptainer/persistent"
+SCRIPTS_DIR="$BOOM_DIR/apptainer/scripts"
+CONFIG_FILE="$BOOM_DIR/config.yaml"
+SIF_DIR="$BOOM_DIR/apptainer/sif"
 
 mkdir -p "$PERSISTENT_DIR/mongodb"
 mkdir -p "$PERSISTENT_DIR/valkey"
@@ -15,9 +14,13 @@ mkdir -p "$PERSISTENT_DIR/alerts"
 mkdir -p "$PERSISTENT_DIR/kafka_data"
 
 # Clear log files or create them if they don't exist
-: > "$LOGS_DIR/scheduler.log"
 mkdir -p "$LOGS_DIR/kafka"
 mkdir -p "$LOGS_DIR/valkey"
+mkdir -p "$LOGS_DIR/otel-collector"
+mkdir -p "$LOGS_DIR/prometheus"
+: > "$LOGS_DIR/scheduler.log"
+: > "$LOGS_DIR/otel-collector/otel-collector.log"
+: > "$LOGS_DIR/prometheus/prometheus.log"
 
 current_datetime() {
     TZ=utc date "+%Y-%m-%d %H:%M:%S"
@@ -61,5 +64,23 @@ apptainer instance start \
     --bind "$PERSISTENT_DIR/kafka_data:/var/lib/kafka/data" \
     "$SIF_DIR/kafka.sif" broker
 $SCRIPTS_DIR/kafka-healthcheck.sh # Wait for Kafka to be ready
+
+# -----------------------------
+# 4. Monitoring services
+# -----------------------------
+echo "$(current_datetime) - Starting Otel Collector"
+apptainer exec \
+  --bind "$BOOM_DIR/config/apptainer-otel-collector-config.yaml:/etc/otelcol/config.yaml" \
+  --bind "$LOGS_DIR/otel-collector:/var/log/otel-collector" \
+  "$SIF_DIR/otel-collector.sif" /otelcol --config /etc/otelcol/config.yaml \
+  > "$LOGS_DIR/otel-collector/otel-collector.log" 2>&1 &
+
+echo "$(current_datetime) - Starting Prometheus"
+apptainer exec \
+  --bind "$BOOM_DIR/config/prometheus.yaml:/etc/prometheus/prometheus.yaml" \
+  --bind "$LOGS_DIR/prometheus:/prometheus" \
+  "$SIF_DIR/prometheus.sif" \
+  /bin/prometheus --web.enable-otlp-receiver --config.file=/etc/prometheus/prometheus.yaml \
+  > "$LOGS_DIR/prometheus/prometheus.log" 2>&1 &
 
 echo "$(current_datetime) - BOOM services started successfully"
