a6c987f874bfea58c1ea07e502ec63d23e42a61b
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-09-08 23:12:36 +0000

    Add kuma to monitor active instance.

--- apptainer/def/build-sif.sh
+++ apptainer/def/build-sif.sh
@@ -5,3 +5,4 @@ apptainer build apptainer/sif/kafka.sif apptainer/def/kafka.def
 apptainer build apptainer/sif/boom.sif apptainer/def/boom.def
 apptainer build apptainer/sif/otel-collector.sif docker://otel/opentelemetry-collector:0.131.1
 apptainer build apptainer/sif/prometheus.sif docker://prom/prometheus:latest
+apptainer build apptainer/sif/uptime-kuma.sif apptainer/def/uptime-kuma.def
--- apptainer/scripts/apptainer_compose.sh
+++ apptainer/scripts/apptainer_compose.sh
@@ -12,16 +12,17 @@ mkdir -p "$PERSISTENT_DIR/mongodb"
 mkdir -p "$PERSISTENT_DIR/valkey"
 mkdir -p "$PERSISTENT_DIR/alerts"
 mkdir -p "$PERSISTENT_DIR/kafka_data"
+mkdir -p "$PERSISTENT_DIR/uptime-kuma"
 
 # Clear log files or create them if they don't exist
 mkdir -p "$LOGS_DIR/kafka"
 mkdir -p "$LOGS_DIR/valkey"
-mkdir -p "$LOGS_DIR/otel-collector"
-mkdir -p "$LOGS_DIR/prometheus"
+mkdir -p "$LOGS_DIR/monitoring"
 : > "$LOGS_DIR/consumer.log"
 : > "$LOGS_DIR/scheduler.log"
-: > "$LOGS_DIR/otel-collector/otel-collector.log"
-: > "$LOGS_DIR/prometheus/prometheus.log"
+: > "$LOGS_DIR/monitoring/otel-collector.log"
+: > "$LOGS_DIR/monitoring/prometheus.log"
+: > "$LOGS_DIR/monitoring/uptime-kuma.log"
 
 current_datetime() {
     TZ=utc date "+%Y-%m-%d %H:%M:%S"
@@ -33,7 +34,7 @@ echo "$(current_datetime) - Starting BOOM services with Apptainer"
 # 1. MongoDB
 # -----------------------------
 echo "$(current_datetime) - Starting MongoDB"
-apptainer instance start --bind "$PERSISTENT_DIR/mongodb:/data/db" "$SIF_DIR/mongo.sif" mongo
+apptainer instance run --bind "$PERSISTENT_DIR/mongodb:/data/db" "$SIF_DIR/mongo.sif" mongo
 sleep 5
 $SCRIPTS_DIR/mongodb-healthcheck.sh # Wait for MongoDB to be ready
 
@@ -41,28 +42,17 @@ $SCRIPTS_DIR/mongodb-healthcheck.sh # Wait for MongoDB to be ready
 # 2. Valkey
 # -----------------------------
 echo "$(current_datetime) - Starting Valkey"
-apptainer instance start \
-  --bind "$PERSISTENT_DIR/valkey:/data" \
-  --bind "$LOGS_DIR/valkey:/valkey/logs" \
-  "$SIF_DIR/valkey.sif" valkey
+apptainer instance run "$SIF_DIR/valkey.sif" valkey
 $SCRIPTS_DIR/valkey-healthcheck.sh # Wait for Valkey to be ready
 
 # -----------------------------
 # 3. Kafka broker
 # -----------------------------
 echo "$(current_datetime) - Starting Kafka broker"
-if [ ! -f "/tmp/kraft-combined-logs/meta.properties" ]; then # Generate meta.properties if it doesn't exist
-  echo "$(current_datetime) - Generating Kafka meta.properties file"
-  apptainer exec "$SIF_DIR/kafka.sif" \
-  /opt/kafka/bin/kafka-storage.sh format \
-    --config /opt/kafka/config/server.properties \
-    --cluster-id "$(uuidgen)" \
-    --ignore-formatted \
-    --standalone
-fi
-apptainer instance start \
-    --bind "$LOGS_DIR/kafka:/opt/kafka/logs" \
+apptainer instance run \
     --bind "$PERSISTENT_DIR/kafka_data:/var/lib/kafka/data" \
+    --bind "$PERSISTENT_DIR/kafka_data:/opt/kafka/config" \
+    --bind "$LOGS_DIR/kafka:/opt/kafka/logs" \
     "$SIF_DIR/kafka.sif" broker
 $SCRIPTS_DIR/kafka-healthcheck.sh # Wait for Kafka to be ready
 
@@ -78,19 +68,23 @@ apptainer instance start \
 # -----------------------------
 # 5. Monitoring services
 # -----------------------------
-#echo "$(current_datetime) - Starting Otel Collector"
-#apptainer exec \
-#  --bind "$BOOM_DIR/config/apptainer-otel-collector-config.yaml:/etc/otelcol/config.yaml" \
-#  --bind "$LOGS_DIR/otel-collector:/var/log/otel-collector" \
-#  "$SIF_DIR/otel-collector.sif" /otelcol --config /etc/otelcol/config.yaml \
-#  > "$LOGS_DIR/otel-collector/otel-collector.log" 2>&1 &
-#
-#echo "$(current_datetime) - Starting Prometheus"
-#apptainer exec \
-#  --bind "$BOOM_DIR/config/prometheus.yaml:/etc/prometheus/prometheus.yaml" \
-#  --bind "$LOGS_DIR/prometheus:/prometheus" \
-#  "$SIF_DIR/prometheus.sif" \
-#  /bin/prometheus --web.enable-otlp-receiver --config.file=/etc/prometheus/prometheus.yaml \
-#  > "$LOGS_DIR/prometheus/prometheus.log" 2>&1 &
+echo "$(current_datetime) - Starting Prometheus"
+apptainer exec \
+  --bind "$BOOM_DIR/config/prometheus.yaml:/etc/prometheus/prometheus.yaml" \
+  "$SIF_DIR/prometheus.sif" \
+  /bin/prometheus --web.enable-otlp-receiver --config.file=/etc/prometheus/prometheus.yaml \
+  > "$LOGS_DIR/monitoring/prometheus.log" 2>&1 &
+
+echo "$(current_datetime) - Starting Otel Collector"
+apptainer exec \
+  --bind "$BOOM_DIR/config/apptainer-otel-collector-config.yaml:/etc/otelcol/config.yaml" \
+  --bind "$LOGS_DIR/monitoring:/var/log/otel-collector" \
+  "$SIF_DIR/otel-collector.sif" /otelcol --config /etc/otelcol/config.yaml \
+  > "$LOGS_DIR/monitoring/otel-collector.log" 2>&1 &
+
+apptainer instance start \
+  --bind "$PERSISTENT_DIR/uptime-kuma:/app/data" \
+  --bind "$LOGS_DIR/monitoring:/app/logs" \
+  "$SIF_DIR/uptime-kuma.sif" kuma
 
 echo "$(current_datetime) - BOOM services started successfully"
