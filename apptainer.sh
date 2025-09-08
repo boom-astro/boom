b0c4b71c876a30b92c3c85b6e6331db8196b20f4
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-09-08 03:49:09 +0000

    Create a tooling ssh file to control apptainer.

---
+++ apptainer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Description: Script to manage Apptainer instances for MongoDB, Kafka, Valkey, and Boom.
+# Usage: ./apptainer.sh {compose|health|compose}
+
+if [ "$1" != "stop" ] && [ "$1" != "compose" ] && [ "$1" != "health" ]; then
+  echo "Usage: $0 {stop|compose|health}"
+  exit 1
+fi
+
+if [ "$1" = "compose" ]; then
+  ./apptainer_compose.sh
+  exit 0
+fi
+
+if [ "$1" == "stop" ]; then
+  apptainer instance stop mongo
+  apptainer instance stop broker
+  apptainer instance stop valkey
+  apptainer instance stop boom
+fi
+
+if [ "$1" == "health" ]; then
+  apptainer instance list
+  SCRIPTS_DIR="$HOME/boom/apptainer/scripts"
+  $SCRIPTS_DIR/mongodb-healthcheck.sh
+  $SCRIPTS_DIR/valkey-healthcheck.sh
+  $SCRIPTS_DIR/kafka-healthcheck.sh
+fi
