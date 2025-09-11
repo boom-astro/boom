5706daaf7acb67aeb9a30df395bdc9fdf9bf1f3a
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-09-11 12:31:33 -0500

    Improve apptainer.sh to allow starting/ stopping each service.

--- apptainer.sh
+++ apptainer.sh
@@ -1,89 +1,122 @@
 #!/bin/bash
 
-# Script to manage Apptainer instances for MongoDB, Kafka, Valkey, and Boom.
-# $1 = action: build | compose | start | health | stop
-# $2-$6 = arguments used only with the 'start' action:
-#   $2 = survey name for the consumer and scheduler
-#   $3 = logs folder for consumer and scheduler (optional, default: $HOME/boom/logs/boom)
-#   $4 = date for the consumer (optional)
-#   $5 = program ID for the consumer (optional)
-#   $6 = config path for the scheduler (optional)
+# Script to manage Boom using Apptainer.
+# $1 = action: build | start | health | stop
 
 SCRIPTS_DIR="$HOME/boom/apptainer/scripts"
-GREEN="\e[32m"
-RED="\e[31m"
-END="\e[0m"
 
-if [ "$1" != "build" ] && [ "$1" != "compose" ] && [ "$1" != "start" ] && [ "$1" != "health" ] && [ "$1" != "stop" ]; then
+if [ "$1" != "build" ] && [ "$1" != "start" ] && [ "$1" != "health" ] && [ "$1" != "stop" ]; then
   echo "Usage: $0 {build|compose|start|health|stop}"
   exit 1
 fi
 
+kill_process() {
+  local BLUE="\033[0;34m"
+  local END="\033[0m"
+  local process="$1"
+  local name="$2"
+  if pgrep -f "$process" > /dev/null; then
+    pkill -f "$process"
+    echo -e "${BLUE}INFO${END}:    Stopping $name process"
+  fi
+}
+
+stop_service() {
+    local service="$1"
+    local target="$2"
+    if [[ -z "$target" || "$target" = "all" || "$target" = "$service" ]]; then
+        return 0
+    fi
+    return 1
+}
+
+# -----------------------------
+# 1. Build SIF files
+# -----------------------------
 if [ "$1" = "build" ]; then
   ./apptainer/def/build-sif.sh
   exit 0
 fi
 
-if [ "$1" = "compose" ]; then
-  ./apptainer/scripts/apptainer_compose.sh
-  exit 0
-fi
-
+# -----------------------------
+# 2. Start services
+# -----------------------------
 if [ "$1" == "start" ]; then
-  SURVEY=$2
-  LOGS_FOLDER=${3:-"$HOME/boom/logs/boom"}
-  if [ -z "$SURVEY" ]; then
-    echo -e "${RED}Missing required argument: survey name${END}"
-    exit 1
-  fi
-
-  if pgrep -f "/app/kafka_consumer" > /dev/null; then
-    echo -e "${RED}Boom consumer already running.${END}"
-  else
-    ARGS=("$SURVEY")
-    [ -n "$4" ] && ARGS+=("$4") # $4=date
-    [ -n "$5" ] && ARGS+=("$5") # $5=program ID
-    apptainer exec --env-file env instance://boom /app/kafka_consumer "${ARGS[@]}" > "$LOGS_FOLDER/consumer.log" 2>&1 &
-    echo -e "${GREEN}Boom consumer started for survey $SURVEY${END}"
-  fi
-
-  if pgrep -f "/app/scheduler" > /dev/null; then
-    echo -e "${RED}Boom scheduler already running.${END}"
-  else
-    ARGS=("$SURVEY")
-    [ -n "$6" ] && ARGS+=("$6") # $6=config path
-    apptainer exec instance://boom /app/scheduler "${ARGS[@]}" > "$LOGS_FOLDER/scheduler.log" 2>&1 &
-    echo -e "${GREEN}Boom scheduler started for survey $SURVEY${END}"
-  fi
+  ARGS=()
+  [ -n "$2" ] && ARGS+=("$2") # $2=service to start
+  [ -n "$3" ] && ARGS+=("$3") # $3=survey name
+  [ -n "$4" ] && ARGS+=("$4") # $4=logs folder
+  [ -n "$5" ] && ARGS+=("$5") # $5=date
+  [ -n "$6" ] && ARGS+=("$6") # $6=program ID
+  [ -n "$7" ] && ARGS+=("$7") # $7=scheduler config path
+  # See apptainer_start.sh for the full explanation of each argument
+  "$SCRIPTS_DIR/apptainer_start.sh" "${ARGS[@]}"
 fi
 
+# -----------------------------
+# 3. Stop services
+# -----------------------------
+# Arguments for 'stop':
+# $2 = service to stop:
+#      - boom        : stop the Boom instance
+#      - consumer    : stop the consumer process
+#      - scheduler   : stop the scheduler process
+#      - mongo       : stop the MongoDB instance
+#      - broker      : stop the kafka broker instance
+#      - valkey      : stop the Valkey instance
+#      - prometheus  : stop the Prometheus instance
+#      - otel        : stop the OpenTelemetry Collector process
+#      - listener    : stop the Boom healthcheck listener process
+#      - kuma        : stop the Kuma service instance
+#      If not specified or 'all', all services will be stopped.
 if [ "$1" == "stop" ]; then
-  apptainer instance stop kuma
-  if pgrep -f "boom-healthcheck-listener.py" > /dev/null; then
-    pkill -f "boom-healthcheck-listener.py"
-    echo "INFO:    Stopping boom healthcheck listener process"
-  fi # Kill boom-healthcheck-listener process if running
-  if pgrep -f "otel-collector" > /dev/null; then
-    pkill -f "otel-collector"
-    echo "INFO:    Stopping otel-collector process"
-  fi # Kill otel-collector process if running
-  apptainer instance stop prometheus
-  apptainer instance stop boom
-  apptainer instance stop valkey
-  apptainer instance stop broker
-  apptainer instance stop mongo
+  target="$2" # $2=service to stop (all if empty or "all")
+
+  if stop_service "kuma" "$target"; then
+    apptainer instance stop kuma
+  fi
+  if stop_service "listener" "$target"; then
+    kill_process "boom-healthcheck-listener.py" boom healthcheck listener
+  fi
+  if stop_service "otel" "$target"; then
+    kill_process "otel-collector" Otel collector
+  fi
+  if stop_service "prometheus" "$target"; then
+    apptainer instance stop prometheus
+  fi
+  if stop_service "boom" "$target"; then
+    apptainer instance stop boom
+  fi
+  if stop_service "consumer" "$target"; then
+    kill_process "/app/kafka_consumer" consumer
+  fi
+  if stop_service "scheduler" "$target"; then
+    kill_process "/app/scheduler" scheduler
+  fi
+  if stop_service "valkey" "$target"; then
+    apptainer instance stop valkey
+  fi
+  if stop_service "broker" "$target"; then
+    apptainer instance stop broker
+  fi
+  if stop_service "mongo" "$target"; then
+    apptainer instance stop mongo
+  fi
 fi
 
+# -----------------------------
+# 4. Health checks
+# -----------------------------
 if [ "$1" == "health" ]; then
   apptainer instance list
   echo
-  $SCRIPTS_DIR/mongodb-healthcheck.sh 0
-  $SCRIPTS_DIR/valkey-healthcheck.sh 0
-  $SCRIPTS_DIR/kafka-healthcheck.sh 0
-  $SCRIPTS_DIR/process-healthcheck.sh "/app/kafka_consumer" consumer
-  $SCRIPTS_DIR/process-healthcheck.sh "/app/scheduler" scheduler
-  $SCRIPTS_DIR/prometheus-healthcheck.sh 0
-  $SCRIPTS_DIR/process-healthcheck.sh "/otelcol" otel-collector
-  $SCRIPTS_DIR/boom-listener-healthcheck.sh 0
-  $SCRIPTS_DIR/kuma-healthcheck.sh 0
+  "$SCRIPTS_DIR/mongodb-healthcheck.sh" 0
+  "$SCRIPTS_DIR/valkey-healthcheck.sh" 0
+  "$SCRIPTS_DIR/kafka-healthcheck.sh" 0
+  "$SCRIPTS_DIR/process-healthcheck.sh" "/app/kafka_consumer" consumer
+  "$SCRIPTS_DIR/process-healthcheck.sh" "/app/scheduler" scheduler
+  "$SCRIPTS_DIR/prometheus-healthcheck.sh" 0
+  "$SCRIPTS_DIR/process-healthcheck.sh" "/otelcol" otel-collector
+  "$SCRIPTS_DIR/boom-listener-healthcheck.sh" 0
+  "$SCRIPTS_DIR/kuma-healthcheck.sh" 0
 fi
