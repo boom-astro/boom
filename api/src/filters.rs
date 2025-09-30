33c31c90404822dd9338944b796d7df36e5cd7fa
Theophile du Laz <theophile.dulaz@gmail.com>
2025-09-30 09:18:37 +0200

    API: Pipeline Query endpoint (#248)
    This PR addresses part of #166, by adding a `post_pipeline_query`
    endpoint, that lets a user of the API run a MongoDB aggregation pipeline
    on a collection.
    
    Also, this PR reorganizes the `queries.rs` file into a `queries` module
    where we have a file per "category" of endpoint, to keep each feature a
    little more self-contained and the code readable.
    
    ---------
    
    Co-authored-by: Pete Bachant <pbachant@caltech.edu>

--- api/src/filters.rs
+++ api/src/filters.rs
@@ -1,39 +1,55 @@
+use mongodb::bson::Document;
+
 /// Functionality for working with filters
 
 // Deserialize helper functions
-fn _deserialize_filter(
-    filter: &serde_json::Value,
-) -> Result<mongodb::bson::Document, std::io::Error> {
-    match filter {
+fn _deserialize_filter(mongo_filter_json: &serde_json::Value) -> Result<Document, std::io::Error> {
+    match mongo_filter_json {
         serde_json::Value::Object(_) => {
-            match mongodb::bson::to_document(&filter) {
+            match mongodb::bson::to_document(&mongo_filter_json) {
                 Ok(doc) => return Ok(doc),
                 Err(e) => {
                     return Err(std::io::Error::new(
                         std::io::ErrorKind::InvalidInput,
-                        format!("Invalid filter: {:?}", e),
+                        format!("Invalid MongoDB Filter: {:?}", e),
                     ));
                 }
             };
         }
         _ => Err(std::io::Error::new(
             std::io::ErrorKind::InvalidInput,
-            "Filter must be a JSON object",
+            "MongoDB Filter must be a JSON object",
         )),
     }
 }
 
 /// Parse a filter from a JSON value
-pub fn parse_filter(filter: &serde_json::Value) -> Result<mongodb::bson::Document, std::io::Error> {
-    _deserialize_filter(filter)
+pub fn parse_filter(mongo_filter_json: &serde_json::Value) -> Result<Document, std::io::Error> {
+    _deserialize_filter(mongo_filter_json)
 }
 
 /// Parse an optional filter from a JSON value
 pub fn parse_optional_filter(
-    filter_opt: &Option<serde_json::Value>,
-) -> Result<mongodb::bson::Document, std::io::Error> {
-    match filter_opt {
+    mongo_filter_json_opt: &Option<serde_json::Value>,
+) -> Result<Document, std::io::Error> {
+    match mongo_filter_json_opt {
         Some(filter) => _deserialize_filter(filter),
-        None => Ok(mongodb::bson::Document::new()),
+        None => Ok(Document::new()),
+    }
+}
+
+pub fn parse_pipeline(
+    mongo_pipeline_json: &serde_json::Value,
+) -> Result<Vec<Document>, std::io::Error> {
+    // the value should be an array of Object
+    match mongo_pipeline_json {
+        serde_json::Value::Array(stages) => Ok(stages
+            .iter()
+            .map(|stage| _deserialize_filter(stage))
+            .collect::<Result<Vec<Document>, std::io::Error>>()?),
+        _ => Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Pipeline must be a JSON array",
+        )),
     }
 }
