1c609f0603dd526df2e5d10a76c0c46a320f9696
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-10-08 17:45:11 -0500

    Update cider image classifications to store result as list of field: ["Nuclear", "SNI", "SNII", "Cataclysmic"] This allows to fix the cider classifications not being sent to the kafka after filtering.

--- src/enrichment/ztf.rs
+++ src/enrichment/ztf.rs
@@ -213,7 +213,10 @@ impl EnrichmentWorker for ZtfEnrichmentWorker {
                     "classifications.acai_o": acai_o_scores[0],
                     "classifications.acai_b": acai_b_scores[0],
                     "classifications.btsbot": btsbot_scores[0],
-                    "classifications.cider_img": cider_img_scores,
+                    "classifications.cider_img_nuclear": cider_img_scores[0],
+                    "classifications.cider_img_snI": cider_img_scores[1],
+                    "classifications.cider_img_snII": cider_img_scores[2],
+                    "classifications.cider_img_cataclysmic": cider_img_scores[3],
                     // properties
                     "properties": properties,
                 }
