60d72212cd0c2658ec212ab61d6bfd5f13a30180
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-10-27 14:27:42 -0500

    Merge main to apptainer and update apptainer_run.sh to fit the new _run.sh

--- tests/throughput/apptainer_run.sh
+++ tests/throughput/apptainer_run.sh
@@ -15,6 +15,7 @@ CONFIG_FILE="$TESTS_DIR/throughput/config.yaml"
 
 EXPECTED_ALERTS=29142
 N_FILTERS=25
+TIMEOUT_SECS=300 # 5 minutes
 
 GREEN="\e[32m"
 RED="\e[31m"
@@ -25,6 +26,13 @@ mkdir -p "$PERSISTENT_DIR"
 
 current_datetime() {
     TZ=utc date "+%Y-%m-%d %H:%M:%S"
+}
+
+stop_all_instances() {
+    apptainer instance stop boom
+    apptainer instance stop kafka
+    apptainer instance stop valkey
+    apptainer instance stop mongo
 }
 
 # -----------------------------
@@ -123,34 +131,71 @@ else
 fi
 
 # -----------------------------
-# 8. Wait for alerts ingestion
+# 8. Wait for consumer to start expecting messages
+# -----------------------------
+echo "$(current_datetime) - Waiting for Kafka consumer to start"
+START_TIME=$(date +%s)
+while ! grep -q "Consumer received first message, continuing..." "$LOGS_DIR/consumer.log"; do
+    CURRENT_TIME=$(date +%s)
+    ELAPSED_TIME=$((CURRENT_TIME - START_TIME))
+    if [ $ELAPSED_TIME -ge $TIMEOUT_SECS ]; then
+        echo "$(current_datetime) - Timeout reached while waiting for Kafka consumer to start"
+        stop_all_instances
+        exit 1
+    fi
+    sleep 1
+done
+
+# -----------------------------
+# 9. Wait for alerts ingestion
 # -----------------------------
 echo && echo "$(current_datetime) - Waiting for all alerts to be ingested"
+START_TIME=$(date +%s)
 while [ "$(apptainer exec instance://mongo mongosh "mongodb://mongoadmin:mongoadminsecret@localhost:27017" --quiet --eval "db.getSiblingDB('boom-benchmarking').ZTF_alerts.countDocuments()")" -lt $EXPECTED_ALERTS ]; do
+    CURRENT_TIME=$(date +%s)
+    ELAPSED_TIME=$((CURRENT_TIME - START_TIME))
+    if [ $ELAPSED_TIME -ge $TIMEOUT_SECS ]; then
+        echo "$(current_datetime) - Timeout reached while waiting for alerts to be ingested"
+        stop_all_instances
+        exit 1
+    fi
     sleep 1
 done
 
 echo "$(current_datetime) - Waiting for all alerts to be classified"
+START_TIME=$(date +%s)
 while [ "$(apptainer exec instance://mongo mongosh "mongodb://mongoadmin:mongoadminsecret@localhost:27017" --quiet --eval "db.getSiblingDB('boom-benchmarking').ZTF_alerts.countDocuments({ classifications: { \$exists: true } })")" -lt $EXPECTED_ALERTS ]; do
+    CURRENT_TIME=$(date +%s)
+    ELAPSED_TIME=$((CURRENT_TIME - START_TIME))
+    if [ $ELAPSED_TIME -ge $TIMEOUT_SECS ]; then
+        echo "$(current_datetime) - Timeout reached while waiting for alerts to be classified"
+        stop_all_instances
+        exit 1
+    fi
     sleep 1
 done
 
 echo "$(current_datetime) - Waiting for filters to run on all alerts"
+START_TIME=$(date +%s)
 PASSED_ALERTS=0
 while [ $PASSED_ALERTS -lt $EXPECTED_ALERTS ]; do
     PASSED_ALERTS=$(cat "$LOGS_DIR/scheduler.log" | grep "passed filter" | awk -F'/' '{sum += $NF} END {print sum}')
     PASSED_ALERTS=${PASSED_ALERTS:-0}
     PASSED_ALERTS=$((PASSED_ALERTS / N_FILTERS))
+    CURRENT_TIME=$(date +%s)
+    ELAPSED_TIME=$((CURRENT_TIME - START_TIME))
+    if [ $ELAPSED_TIME -ge $TIMEOUT_SECS ]; then
+        echo "$(current_datetime) - Timeout reached while waiting for filters to run on all alerts"
+        stop_all_instances
+        exit 1
+    fi
     sleep 1
 done
 
 # -----------------------------
-# 8. Stop all instances
+# 10. Stop all instances
 # -----------------------------
 echo "$(current_datetime) - All tasks completed; shutting down BOOM services"
-apptainer instance stop boom
-apptainer instance stop kafka
-apptainer instance stop valkey
-apptainer instance stop mongo
+stop_all_instances
 
 exit 0
