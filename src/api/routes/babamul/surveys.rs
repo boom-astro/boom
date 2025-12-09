9e6cf1cb00e7cc5200f214ec6ed39f70fc166200
Theodlz <theophile.dulaz@gmail.com>
2025-12-09 09:58:10 +0100

    add a basic endpoint to retrieve a list of sources from ZTF, and an endpoint to retrieve cutouts for a given candid

--- src/api/routes/babamul/surveys.rs
+++ src/api/routes/babamul/surveys.rs
@@ -1,5 +1,7 @@
+use crate::alert::{AlertCutout, ZtfCandidate};
 use crate::api::models::response;
 use crate::api::routes::babamul::users::BabamulUser;
+use crate::enrichment::ZtfAlertProperties;
 use actix_web::{get, web, HttpResponse};
 use base64::prelude::*;
 use futures::TryStreamExt;
@@ -218,6 +220,201 @@ pub async fn get_object(
     };
     return response::ok(
         &format!("object found with object_id: {}", object_id),
+        serde_json::json!(resp),
+    );
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, ToSchema)]
+struct ObjectQuery {
+    ra: Option<f64>,
+    dec: Option<f64>,
+    radius_arcsec: Option<f64>,
+    start_jd: Option<f64>,
+    end_jd: Option<f64>,
+    min_magpsf: Option<f64>,
+    max_magpsf: Option<f64>,
+    min_drb: Option<f64>,
+    max_drb: Option<f64>,
+    min_sgscore1: Option<f64>,
+    max_sgscore1: Option<f64>,
+    min_distpsnr1: Option<f64>,
+    max_distpsnr1: Option<f64>,
+}
+
+#[get("/babamul/surveys/{survey_name}/objects")]
+pub async fn get_objects(
+    path: web::Path<String>,
+    query: web::Query<ObjectQuery>,
+    current_user: Option<web::ReqData<BabamulUser>>,
+    db: web::Data<Database>,
+) -> HttpResponse {
+    let _current_user = match current_user {
+        Some(user) => user,
+        None => {
+            return HttpResponse::Unauthorized().body("Unauthorized");
+        }
+    };
+    let survey_name = path.into_inner();
+    if survey_name.to_uppercase() != "ZTF" {
+        return response::bad_request("only ZTF survey is supported for get_objects endpoint");
+    }
+    let mut filter_doc = Document::new();
+    // Build the filter document based on the query parameters
+    if let (Some(ra), Some(dec), Some(radius_arcsec)) = (query.ra, query.dec, query.radius_arcsec) {
+        // Add cone search filter
+        filter_doc.insert(
+            "coordinates.radec_geojson",
+            doc! {
+                "$geoWithin": {
+                    "$centerSphere": [
+                        [ra, dec],
+                        (radius_arcsec / 3600.0) * (std::f64::consts::PI / 180.0)
+                    ]
+                }
+            },
+        );
+    }
+    if let (Some(start_jd), Some(end_jd)) = (query.start_jd, query.end_jd) {
+        filter_doc.insert(
+            "candidate.jd",
+            doc! {
+                "$gte": start_jd,
+                "$lte": end_jd,
+            },
+        );
+    }
+    if let (Some(min_magpsf), Some(max_magpsf)) = (query.min_magpsf, query.max_magpsf) {
+        filter_doc.insert(
+            "candidate.magpsf",
+            doc! {
+                "$gte": min_magpsf,
+                "$lte": max_magpsf,
+            },
+        );
+    }
+    if let (Some(min_drb), Some(max_drb)) = (query.min_drb, query.max_drb) {
+        filter_doc.insert(
+            "classifications.drb",
+            doc! {
+                "$gte": min_drb,
+                "$lte": max_drb,
+            },
+        );
+    }
+    if let (Some(min_sgscore1), Some(max_sgscore1), Some(min_distpsnr1), Some(max_distpsnr1)) = (
+        query.min_sgscore1,
+        query.max_sgscore1,
+        query.min_distpsnr1,
+        query.max_distpsnr1,
+    ) {
+        filter_doc.insert(
+            "classifications.sgscore1",
+            doc! {
+                "$gte": min_sgscore1,
+                "$lte": max_sgscore1,
+            },
+        );
+        filter_doc.insert(
+            "classifications.distpsnr1",
+            doc! {
+                "$gte": min_distpsnr1,
+                "$lte": max_distpsnr1,
+            },
+        );
+    }
+
+    let projection_doc = doc! {
+        "candid": "_id",
+        "objectId": 1,
+        "candidate": 1,
+        "properties": 1,
+        "classifications": 1,
+    };
+
+    #[derive(Debug, serde::Deserialize, serde::Serialize)]
+    pub struct EnrichedZtfAlert {
+        // #[serde(rename = "_id")]
+        pub candid: i64,
+        #[serde(rename = "objectId")]
+        pub object_id: String,
+        pub candidate: ZtfCandidate,
+        pub properties: ZtfAlertProperties,
+        pub classifications: Document,
+    }
+
+    let alerts_collection: Collection<EnrichedZtfAlert> = db.collection("ZTF_alerts");
+    // Query the alerts collection with the constructed filter
+    let mut alert_cursor = match alerts_collection
+        .find(filter_doc)
+        .projection(projection_doc)
+        .sort(doc! { "_id": 1 })
+        .await
+    {
+        Ok(cursor) => cursor,
+        Err(error) => {
+            return response::internal_error(&format!(
+                "error retrieving alerts for objects: {}",
+                error
+            ));
+        }
+    };
+
+    let mut results: Vec<EnrichedZtfAlert> = Vec::new();
+    while let Some(alert_doc) = match alert_cursor.try_next().await {
+        Ok(Some(doc)) => Some(doc),
+        Ok(None) => None,
+        Err(error) => {
+            return response::internal_error(&format!("error getting documents: {}", error));
+        }
+    } {
+        results.push(alert_doc);
+    }
+    return response::ok(
+        &format!("found {} objects matching query", results.len()),
+        serde_json::json!(results),
+    );
+}
+
+// we also want an endpoint that given a candid, grabs the cutouts from the cutouts collection
+#[get("/babamul/surveys/{survey_name}/objects/cutouts/{candid}")]
+pub async fn get_object_cutouts(
+    path: web::Path<(String, i64)>,
+    current_user: Option<web::ReqData<BabamulUser>>,
+    db: web::Data<Database>,
+) -> HttpResponse {
+    let _current_user = match current_user {
+        Some(user) => user,
+        None => {
+            return HttpResponse::Unauthorized().body("Unauthorized");
+        }
+    };
+    let (survey_name, candid) = path.into_inner();
+    let survey_name = survey_name.to_uppercase(); // All alert collections are uppercase
+
+    let cutout_collection: Collection<AlertCutout> =
+        db.collection(&format!("{}_alerts_cutouts", survey_name));
+    let cutouts = match cutout_collection
+        .find_one(doc! {
+            "_id": candid,
+        })
+        .await
+    {
+        Ok(Some(cutouts)) => cutouts,
+        Ok(None) => {
+            return response::not_found(&format!("no cutouts found for candid {}", candid));
+        }
+        Err(error) => {
+            return response::internal_error(&format!("error getting documents: {}", error));
+        }
+    };
+    let resp = serde_json::json!({
+        "candid": candid,
+        "cutout_science": BASE64_STANDARD.encode(&cutouts.cutout_science),
+        "cutout_template": BASE64_STANDARD.encode(&cutouts.cutout_template),
+        "cutout_difference": BASE64_STANDARD.encode(&cutouts.cutout_difference),
+    });
+    return response::ok(
+        &format!("cutouts found for candid: {}", candid),
         serde_json::json!(resp),
     );
 }
