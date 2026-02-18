a6f4966bee0ce07dae50bfed2550d89fcb9109c0
Theodlz <theophile.dulaz@gmail.com>
2026-02-18 22:14:47 +0100

    fix api.rs (using new endpoint function name)

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -64,7 +64,7 @@ async fn main() -> std::io::Result<()> {
                     .service(routes::babamul::surveys::get_object_xmatches)
                     .service(routes::babamul::surveys::get_objects)
                     .service(routes::babamul::surveys::cone_search_objects)
-                    .service(routes::babamul::surveys::get_alert_cutouts)
+                    .service(routes::babamul::surveys::get_cutouts)
                     .service(routes::babamul::surveys::get_alerts)
                     .service(routes::babamul::surveys::cone_search_alerts)
                     .service(routes::babamul::tokens::get_tokens)
