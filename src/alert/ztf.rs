baba3dc9ac58fc9f496ef766a920016a440bd2bf
Theodlz <theophile.dulaz@gmail.com>
2026-02-19 11:39:41 +0100

    add another debug print

--- src/alert/ztf.rs
+++ src/alert/ztf.rs
@@ -1139,6 +1139,7 @@ impl AlertWorker for ZtfAlertWorker {
             };
             let result = self.insert_aux(&obj, &self.alert_aux_collection).await;
             if let Err(AlertError::AlertAuxExists) = result {
+                warn!("Alert aux document for object_id {} already exists. This can happen when multiple alerts with the same object_id are processed concurrently. Attempting to update the existing document.", object_id);
                 let existing_alert_aux: Option<AlertAuxForUpdate> =
                     self.get_existing_aux(object_id.clone()).await?;
                 self.update_aux_with_retries(
