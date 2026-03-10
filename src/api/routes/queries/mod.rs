3266193e643b4518c5331fe8d117c7024b461218
Thomas Culino <thomas.culino@gmail.com>
2026-03-10 13:37:25 -0700

    Add cutout endpoint to boom API and unit test

--- src/api/routes/queries/mod.rs
+++ src/api/routes/queries/mod.rs
@@ -1,9 +1,11 @@
 pub mod cone_search;
+pub mod cutouts;
 pub mod count;
 pub mod find;
 pub mod pipeline;
 
 pub use cone_search::post_cone_search_query;
+pub use cutouts::get_cutouts;
 pub use count::{post_count_query, post_estimated_count_query};
 pub use find::post_find_query;
 pub use pipeline::post_pipeline_query;
