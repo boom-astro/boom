5ce8bd8e7e065f3e4798306513e9e37e1576c90b
antoine-le-calloch <antoine.lecalloch@gmail.com>
2026-06-11 00:07:17 -0400

    Enforce user ACLs in cone search query so watchlist catalogs query are accessible to authorized users.

--- src/api/routes/queries/cone_search.rs
+++ src/api/routes/queries/cone_search.rs
@@ -2,6 +2,7 @@
 use crate::api::catalogs::catalog_accessible;
 use crate::api::filters::parse_optional_filter;
 use crate::api::models::response;
+use crate::api::routes::users::User;
 
 use actix_web::{post, web, HttpResponse};
 use futures::TryStreamExt;
@@ -123,9 +124,14 @@ impl ConeSearchQuery {
 pub async fn post_cone_search_query(
     db: web::Data<Database>,
     body: web::Json<ConeSearchQuery>,
+    current_user: Option<web::ReqData<User>>,
 ) -> HttpResponse {
+    let current_user = match current_user {
+        Some(user) => user,
+        None => return HttpResponse::Unauthorized().body("Unauthorized"),
+    };
     let catalog_name = body.catalog_name.trim();
-    if !catalog_accessible(&db, catalog_name, None).await {
+    if !catalog_accessible(&db, catalog_name, Some(&current_user)).await {
         return response::not_found(&format!("Catalog {} does not exist", catalog_name));
     }
     let collection_name = catalog_name.to_string();
