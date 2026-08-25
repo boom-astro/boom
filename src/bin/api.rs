9c8c1e8d4fb4692ff35c175a419724ab59aab2b1
sanjeev-sridhar-28 <ssridha1@macalester.edu>
2026-08-25 09:36:24 -0400

    Add get_realtime_stats service to API

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -119,6 +119,7 @@ async fn main() -> std::io::Result<()> {
                     .service(routes::babamul::surveys::get_cutouts)
                     .service(routes::babamul::surveys::get_alerts)
                     .service(routes::babamul::surveys::cone_search_alerts)
+                    .service(routes::babamul::stats::get_realtime_stats)
                     .service(routes::babamul::stats::get_nightly_stats)
                     .service(routes::babamul::stats::get_collection_stats)
                     .service(routes::babamul::stats::get_kafka_stats)
